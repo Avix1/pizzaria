15bce536d3ed3ff46ab335b0676fb1ebc2f58b7d
Leonardo Deodato <odranoelstc2@gmail.com>
2021-10-28 21:13:41 -0300

    Parte 2

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,11 +1,11 @@
 import { extendTheme } from '@chakra-ui/react'
 
 export const theme = extendTheme({
-    style: {
+    styles: {
         global: {
             body: {
                 bg: 'red.400',
-                 
+                color: 'gray.50'
             }
         }
     }
